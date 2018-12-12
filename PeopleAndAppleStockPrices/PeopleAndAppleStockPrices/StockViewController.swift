eb104c3867b0914cb6153a86320263eb37bfc995
onielrosario <onielrosario@pursuit.org>
2018-12-12 16:55:29 -0500

    working on randomizer bug

--- PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/StockViewController.swift
+++ PeopleAndAppleStockPrices/PeopleAndAppleStockPrices/StockViewController.swift
@@ -36,29 +36,30 @@ class StockViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let destination = segue.destination as? StockDetailViewController,
             let selectedStock = stockTableView.indexPathForSelectedRow else { return }
-        let stockCell = allStock[selectedStock.row]
+        let sortedStocks = allStock.sorted{$0.year < $1.year }
+        let stockCell = sortedStocks[selectedStock.row]
         destination.stockInfo = stockCell
-        if stockCell.change > 0 {
+        if stockCell.open > stockCell.close {
             destination.image = UIImage.init(named: "thumbUp")
             destination.view.backgroundColor = .green
         } else {
             destination.image = UIImage.init(named: "thumbsDown")
             destination.view.backgroundColor = .red
         }
-        
     }
     func loadData() {
         if let path = Bundle.main.path(forResource: "applstockinfo", ofType: "json") {
             let myUrl = URL.init(fileURLWithPath: path)
             if let data = try? Data(contentsOf: myUrl) {
                 do {
-                    self.allStock = try JSONDecoder().decode([AppleStocks].self, from: data)
+                    self.allStock = try JSONDecoder().decode([AppleStocks].self, from: data).sorted{$0.year < $1.year }
                 } catch {
                     print(error)
                 }
                 return
             }
         }
+        
     }
     
     private func filterStockDate(date: (month: String, year: String)) -> [AppleStocks] {
@@ -66,7 +67,6 @@ class StockViewController: UIViewController {
             let stockDate = getDateMonth(dateString: stock.date)
             return stockDate.year == date.year && stockDate.month == date.month
         }
-        
         return results
     }
     
@@ -78,9 +78,7 @@ class StockViewController: UIViewController {
             allDateSet.insert("\(formattedDate.year)-\(formattedDate.month)")
         }
         
-        allDates = allDateSet.map { getDateMonth(dateString: $0) }.sorted {
-            return Int($0.year)! < Int($1.year)! && Int($0.month)! < Int($1.month)!
-        }
+        allDates = allDateSet.sorted{ $0 < $1 }.map{ getDateMonth(dateString: $0) }
     }
     
     func getDateMonth(dateString: String) -> (month: String, year: String) {
@@ -89,48 +87,44 @@ class StockViewController: UIViewController {
     }
     
 }
-
-
-
-extension StockViewController: UITableViewDataSource {
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return allDates.count
-    }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return filterStockDate(date: allDates[section]).count
-    }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = stockTableView.dequeueReusableCell(withIdentifier: "stockCell", for: indexPath) as? StockTableViewCell else { return UITableViewCell()}
-        
-        let sortedStock = filterStockDate(date: allDates[indexPath.section]).sorted {
-            $0.month < $1.month}
-        let stock = sortedStock[indexPath.row]
-        
-        
-      //  let indexPath = allStock[indexPath.row]
-        
-
-        cell.stockOpening.text = "\(stock.date)"
-        cell.stockClosing.text = "\(stock.open)"
-        if stock.change > 0 {
-            cell.backgroundColor = .green
-            cell.stockImage.image = UIImage.init(named: "thumbUp")
-        } else {
-            cell.backgroundColor = .red
-            cell.stockImage.image = UIImage.init(named: "thumbsDown")
+    
+    
+    extension StockViewController: UITableViewDataSource {
+        func numberOfSections(in tableView: UITableView) -> Int {
+            return allDates.count
         }
-        return cell
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            let sections = filterStockDate(date: allDates[section]).sorted{$0.year < $1.year}
+            
+            return sections.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            guard let cell = stockTableView.dequeueReusableCell(withIdentifier: "stockCell", for: indexPath) as? StockTableViewCell else { return UITableViewCell()}
+            let sortedStock = filterStockDate(date: allDates[indexPath.section]).sorted {
+                $0.month < $1.month}
+            let stock = sortedStock[indexPath.row]
+            cell.stockOpening.text = "\(stock.date)"
+            cell.stockClosing.text = "\(stock.open)"
+            if stock.close > stock.open {
+                cell.backgroundColor = .green
+                cell.stockImage.image = UIImage.init(named: "thumbUp")
+            } else {
+                cell.backgroundColor = .red
+                cell.stockImage.image = UIImage.init(named: "thumbsDown")
+            }
+            return cell
+        }
+        
+        func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+            let date = allDates[section]
+            return "Year \(date.year) Month \(date.month)"
+        }
     }
     
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        let date = allDates[section]
-        return "Year \(date.year) Month \(date.month)"
-    }
-}
-
-extension StockViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 80
-    }
+    extension StockViewController: UITableViewDelegate {
+        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            return 80
+        }
 }
